914cb973b52fa962df4d0524b42c9c8b9d4deb45
Camilla Alves <arcamilla42@gmail.com>
2026-08-21 12:52:51 -0300

    feat: no more get_visualizations, but it's slower

--- main.py
+++ main.py
@@ -10,19 +10,7 @@ class Chain:
             self.content: list[Chain | str] = []
         else:
             self.content = content
-        self.visualizations: set[str] | None = None
-        self.has_all_vis = False
         self.string = ""
-
-    def get_visualizations(self) -> set[str]:
-        if self.visualizations is None:
-            self.visualizations = {str(self)}
-        return self.visualizations
-
-    def append_visualization(self, chain: "Chain") -> None:
-        if self.visualizations is None:
-            self.visualizations = {str(self)}
-        self.visualizations.add(str(chain))
 
     def copy(self) -> "Chain":
         new_content: list[Chain | str] = [
@@ -30,7 +18,6 @@ class Chain:
             for chain in self.content
         ]
         chain = Chain(new_content)
-        chain.visualizations = self.get_visualizations().copy()
         return chain
 
     def rotate(self) -> "Chain":
@@ -53,7 +40,6 @@ class Chain:
                 ]
             )
         )
-        new_chain.append_visualization(new_chain)
         return new_chain
 
     def __str__(self) -> str:
@@ -65,21 +51,8 @@ class Chain:
                 text += str(chain)
         return f"[{text}]"
 
-    def print_visualizations(self) -> None:
-        vis_sort = sorted(self.get_visualizations())
-        for n, vis in enumerate(vis_sort):
-            print(f"    {n + 1}. `{vis}`")
-
-    def list_visualizations(self) -> str:
-        text = ""
-        vis_sort = sorted(self.get_visualizations())
-        for n, vis in enumerate(vis_sort):
-            text += f"    {n + 1}. `{vis}`\n"
-        return text
-
-    def find_all_visualizations(self) -> None:
-        if self.has_all_vis:
-            return
+    def get_visualizations(self) -> "list[str]":
+        visualizations: list[str] = [str(self)]
         stack: list[Chain] = [self]
         while stack:
             chain = stack.pop(0)
@@ -87,16 +60,16 @@ class Chain:
                 continue
             if isinstance(chain.content[0], Chain):
                 enter_chain = chain.enter()
-                if str(enter_chain) not in self.get_visualizations():
-                    self.append_visualization(enter_chain)
+                if str(enter_chain) not in visualizations:
+                    visualizations.append(str(enter_chain))
                     stack.append(enter_chain)
             if len(chain.content) <= 1:
                 continue
             rotate_chain = chain.rotate()
-            if str(rotate_chain) not in self.get_visualizations():
-                self.append_visualization(rotate_chain)
+            if str(rotate_chain) not in visualizations:
+                visualizations.append(str(rotate_chain))
                 stack.append(rotate_chain)
-        self.has_all_vis = True
+        return visualizations
 
     def __eq__(self, other: object) -> bool:
         if not isinstance(other, Chain):
@@ -104,9 +77,12 @@ class Chain:
         return self.unique_str() == other.unique_str()
 
     def unique_str(self) -> str:
-        self.find_all_visualizations()
+        if self.string:
+            return self.string
         vis_sort = sorted(self.get_visualizations(), reverse=True)
-        return str(vis_sort[0])
+        self.string = vis_sort[0]
+        self.visualizations = None
+        return self.string
 
     def __hash__(self) -> int:
         return hash(self.unique_str())
@@ -122,14 +98,10 @@ def convert_it(text: str, index: int) -> tuple[Chain | str, int]:
     while index < text_size:
         char = text[index]
         if char == "]":
-            chain.string = ""
-            chain.visualizations = {str(chain)}
             index += 1
             return chain, index
         new_chain, index = convert_it(text, index)
         chain.content.append(new_chain)
-    chain.string = ""
-    chain.visualizations = {str(chain)}
     return chain, index
 
 
@@ -146,36 +118,42 @@ def explore_keys(initial_chains: set[Chain]) -> None:
         print(f"## KEY {level + 1}\n")
         all_chains: set[Chain] = set()
         for chain in previous_chains:
-            for vis in chain.get_visualizations():
-                sub_chain = convert_text_to_chain(f"{vis[:-1]}K{vis[-1:]}")
+            chain_str = str(chain)
+            for index in range(1,len(chain_str)):
+                sub_chain = convert_text_to_chain(
+                    f"{chain_str[:index]}K{chain_str[index:]}"
+                )
                 all_chains.add(sub_chain)
         print(f"{level + 1} : {len(all_chains)}")
         previous_chains = all_chains
         if len(previous_chains) >= 500000:
-            return 
+            return
     print(time.time() - begin)
 
 
 def main() -> None:
     start_level = 1
-    end_level = 10
+    end_level = 5
     previous_chains: set[Chain] = set()
     for level in range(end_level):
         print(f"\n## CHAINS {level + 1}\n")
         initial_chains: set[Chain] = set()
         if len(previous_chains) == 0:
             initial_chains.add(convert_text_to_chain("[]"))
-        else:
-            for chain in previous_chains:
-                for vis in chain.get_visualizations():
-                    if vis[0] == "(":
-                        continue
-                    sub_chain = convert_text_to_chain(f"{vis[:-1]}[]{vis[-1:]}")
-                    initial_chains.add(sub_chain)
+        for chain in previous_chains:
+            chain_str = str(chain)
+            for index in range(1,len(chain_str)):
+                sub_chain = convert_text_to_chain(
+                    f"{chain_str[:index]}[]{chain_str[index:]}"
+                )
+                initial_chains.add(sub_chain)
         previous_chains = initial_chains
         if level < start_level - 1:
             continue
-
+        explore_keys(initial_chains)
+        if level >= end_level:
+            break
 
 if __name__ == "__main__":
-    main()
+    import cProfile
+    cProfile.run("main()")
